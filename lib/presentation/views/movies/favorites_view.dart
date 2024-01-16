c42a89ccacaa381e26648dc5b56d3eff14691516
dani3571 <ccd2024081@est.univalle.edu>
2024-01-16 16:30:58 -0400

    eliminado imports no usados

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,9 +1,7 @@
-import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
